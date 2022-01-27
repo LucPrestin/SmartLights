4fbf8a5ef58fc9aa9f184f01f6e7b513ce887c8f
LucPrestin <l.prestin@gmx.de>
2022-01-27 16:44:46 +0100

    add config abstraction and an example config

---
+++ strip_configs/three_strips.h
@@ -0,0 +1,10 @@
+#include <vector>
+#include <string>
+#include "../src/stripConfig.hpp"
+#include <Adafruit_NeoPixel.h>
+
+std::vector<StripConfig> configurations {
+    StripConfig(8, 19, NEO_GRB + NEO_KHZ800, "little_frame"),
+    StripConfig(12, 18, NEO_GRB + NEO_KHZ800, "middle_frame"),
+    StripConfig(12, 5, NEO_GRB + NEO_KHZ800, "big_frame")
+}
