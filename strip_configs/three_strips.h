861764922c3c49c0d338d5a84ffd3784fb4654e4
LucPrestin <LucPrestin@users.noreply.github.com>
2022-01-30 22:04:53 +0100

    adapt config file to not use vectors anymore

--- strip_configs/three_strips.h
+++ strip_configs/three_strips.h
@@ -1,10 +1,11 @@
-#include <vector>
 #include <Adafruit_NeoPixel.h>
 
-#define num_stars 3
+#define num_strips 3
 
-std::vector<Adafruit_NeoPixel> strips {
+Adafruit_NeoPixel strips[num_strips] {
   Adafruit_NeoPixel(8, 19, NEO_GRB + NEO_KHZ800),
   Adafruit_NeoPixel(12, 18, NEO_GRB + NEO_KHZ800),
   Adafruit_NeoPixel(12, 5, NEO_GRB + NEO_KHZ800)
-};
+}
+
+uint8_t num_stars[num_strips] {2, 3, 3};
