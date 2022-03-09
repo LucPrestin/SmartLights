5d31fcf418f1f0eff321942241a6bed7865946c1
LucPrestin <40767277+LucPrestin@users.noreply.github.com>
2022-03-09 23:26:43 +0100

    Enhancement/8 wifi access (#9)
    * add wifi config file
    
    * add installation guide to readme
    
    mainly to add advice on how to skip changes to the wifi config
    
    * add connecting to wifi on boot
    
    * add checking for wifi connection in main loop
    
    * add test strip config
    
    * add reconnecting on connection loss and fix color signaling of
    connection status

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,23 @@
 #include <algorithm>
 #include <array>
 
+#include "WiFi.h"
+#include "wifi_config.h"
+
 // here goes the config you want to use
-#include "./strip_configs/three_strips.h"
+#include "./strip_configs/test_strip.h"
+
+void set_all_strips_to(uint32_t color) {
+  for (auto & strip: strips) {
+    strip.fill(color, 0, strip.numPixels());
+  }
+}
+
+void show_all_strips() {
+  for (auto & strip : strips) {
+    strip.show();
+  }
+}
 
 void stars(uint16_t count, uint8_t wait) {
   #define stepSize 1
@@ -38,58 +53,94 @@ void stars(uint16_t count, uint8_t wait) {
           strips[strip_index].setPixelColor(star_indices.at(strip_index).at(star_index), strips[strip_index].Color(255 - step, 255 - step, 0));
         }
       }
-      for (auto & strip: strips) {
-        strip.show();
-      }
+      show_all_strips();
       delay(round(500.0 * wait / 255));
     }       
   }
 }
 
-uint32_t Wheel(Adafruit_NeoPixel & strip, byte WheelPos) {
+uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return Adafruit_NeoPixel::Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return Adafruit_NeoPixel::Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return Adafruit_NeoPixel::Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 void rainbow(uint8_t wait) {
-    uint16_t i, j;
+    uint16_t i;
 
-    for(j = 0; j < 256; j++) {
-      for (auto & strip: strips) {
-        for(i = 0; i < strip.numPixels(); i++) {
-          strip.setPixelColor(i, Wheel(strip, (i + j) & 255));
-        }
-      }
-
-      for (auto & strip: strips) {
-        strip.show();
-      }
-      
+    for(i = 0; i < 256; i++) {
+      set_all_strips_to(Wheel(i & 255));
+      show_all_strips();
       delay(wait);
     }
 }
 
-void setup() {
+void setup_strips() {
   for (auto & strip: strips) {
     strip.begin();
   }
 
-  for (auto & strip: strips) {
-    strip.show();
+  show_all_strips();
+}
+
+void wait_for_connection() {
+  set_all_strips_to(Adafruit_NeoPixel::Color(255, 165, 0));
+  show_all_strips();
+
+  delay(1000);
+
+  auto status = WiFi.status();
+  while (status != WL_CONNECTED && status != WL_CONNECT_FAILED) {
+    delay(1000);
+    status = WiFi.status();
+  }
+
+  switch (status) {
+    case WL_CONNECT_FAILED:
+      set_all_strips_to(Adafruit_NeoPixel::Color(255, 0, 0));
+      show_all_strips();
+      break;
+    case WL_CONNECTED:
+      set_all_strips_to(Adafruit_NeoPixel::Color(0, 255, 0));
+      show_all_strips();
+      delay(2000);
+      set_all_strips_to(Adafruit_NeoPixel::Color(0, 0, 0));
+      show_all_strips();
+      delay(1000);
+      break;
+    default:
+      break;
   }
 }
 
-void loop() {
-    randomSeed(analogRead(1));
+void setup_wifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  wait_for_connection();
+}
 
-    stars(20, 3);
-    rainbow(40);
+void setup() {
+  setup_strips();
+  setup_wifi();
+}
+
+void loop() {
+  currentTime = millis();
+  if ((WiFi.status() != WL_CONNECTED) && (currentTime - previousTime >= timeoutInterval)) {
+    WiFi.reconnect();
+    wait_for_connection();
+    previousTime = currentTime;
+  }
+
+  randomSeed(analogRead(1));
+
+  stars(20, 3);
+  rainbow(40);
 }
---
+++ src/wifi_config.h
@@ -0,0 +1,6 @@
+unsigned long timeoutInterval = 30000;
+unsigned long previousTime = millis();
+unsigned long currentTime = millis();
+
+const char * ssid = "";
+const char * password = "";
